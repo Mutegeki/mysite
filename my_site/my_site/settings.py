579ab33c5d83b2d48e91d9b64ed0ba793630e321
Mutegeki <braynmute@gmail.com>
2018-03-27 06:24:29 -0400

    Added media in the settings.py

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -129,6 +129,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
 
+MEDIA_URL  = '/media'
+MEDIA_ROOT  = os.path.join(BASE_DIR,'madia')
+
+
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_cdn')
 
