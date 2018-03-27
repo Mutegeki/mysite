e246bdf5c88ba45286d1a4d3c268cc681efbf8f2
Mutegeki <braynmute@gmail.com>
2018-03-27 09:47:45 -0400

    updated model in driver and added a file driverlist.html

--- my_site/mycar/models.py
+++ my_site/mycar/models.py
@@ -17,15 +17,15 @@ class Car(models.Model):
 
 
 class Driver(models.Model):
-	first_name=models.CharField(max_length=30,)
+	first_name=models.CharField(max_length=30)
 	last_name=models.CharField(max_length=30)
 	date_of_birth=models.DateField()
-	sex=models.CharField()
+	sex=models.CharField(max_length=10)
 	phone_no=models.CharField(max_length=13, unique=True)
-	permit=models.CharField()
-	nation=models.CharField()
-	district=models.CharField()
-	thumb=models.ImageField( default='default.png',blank=True)
+	permit=models.CharField(max_length=40, unique=True)
+	nation=models.CharField(max_length=15)
+	district=models.CharField(max_length=30)
+	thumb=models.ImageField(default='default.png',blank=True)
 	
 
 	
