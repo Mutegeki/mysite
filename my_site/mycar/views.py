ccba76e8c1004ba14cd3bc162b9529e9306b3f32
Mutegeki <braynmute@gmail.com>
2018-04-05 06:26:24 -0400

    removed time in put

--- my_site/mycar/views.py
+++ my_site/mycar/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.shortcuts import render, redirect
-from.models import Car, Driver
+from.models import Car, Driver, Service
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
