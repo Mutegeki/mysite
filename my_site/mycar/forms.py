c6567bc4973243845cecd9830b62105b9d3f75b3
Mutegeki Brian <Braynmute@gmail.com>
2018-08-31 15:34:33 -0400

    Revert "Mycar"

--- my_site/mycar/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from. import models
-
-
-class CreateCar(forms.ModelForm):
-    class Meta:
-        model = models.Car
-        fields = ['reg_no', 'year_of_manufacture',
-                  'year_of_purchase', 'total_mileage_in_km']
